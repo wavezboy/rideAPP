b09ad6b87346424210af571991990c7e7435af6f
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-10-04 18:46:41 +0100

    ride schema done

---
+++ src/models/rideModel.ts
@@ -0,0 +1,17 @@
+import { InferSchemaType, Schema, model } from "mongoose";
+
+const rideSchema = new Schema(
+  {
+    driver_id: { type: String },
+    passenger_id: { type: String },
+    start_time: { type: String },
+    end_time: { type: String },
+    fare: { type: String },
+    distance: { type: String },
+  },
+  { timestamps: true }
+);
+
+type ride = InferSchemaType<typeof rideSchema>;
+
+export default model<ride>("ride", rideSchema);
