73e1421682e44c5dc753c030bb18723e1005d713
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-10-06 07:28:12 +0100

    register a user as a driver

--- src/controller/driver-controller.ts
+++ src/controller/driver-controller.ts
@@ -1,13 +1,36 @@
 import { RequestHandler } from "express";
+import driverModel from "../models/driverModel";
 
 interface driverBody {
   vehicleInfo: string;
   licensePlate: string;
 }
 
-export const registerDriver: RequestHandler = async (req, res, next) => {
+export const registerDriver: RequestHandler<
+  unknown,
+  unknown,
+  driverBody,
+  unknown
+> = async (req, res, next) => {
+  const { licensePlate, vehicleInfo } = req.body;
+  //   set the userId as the driver id
+  const driver_id = "";
   try {
-    res.status(201).json();
+    // check existingDriver
+    const existingDriver = await driverModel.findOne({});
+
+    if (existingDriver) {
+      res.status(500).json("user is already a driver");
+    }
+
+    const driver = driverModel.create({
+      driver_id: driver_id,
+      vehicle_info: vehicleInfo,
+      availability: false,
+      current_location: null,
+    });
+
+    res.status(201).json(driver);
   } catch (error) {
     next(error);
   }
