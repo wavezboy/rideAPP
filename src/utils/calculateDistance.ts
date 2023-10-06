20a281fb7cc1bacf1fd9421df39885c476448379
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-10-06 18:44:32 +0100

    Haversine formula

--- src/utils/calculateDistance.ts
+++ src/utils/calculateDistance.ts
@@ -4,7 +4,23 @@ const toRadians = (degress) => {
   return (degress * Math.PI) / 180;
 };
 
+// implementation of this function using the Haversine formula for calculating distances
+
 export const calculateDistance = (cords1: location, cords2: location) => {
   const earthRadiusKm = 6371;
-  const dlat = toRadians(cords2.latitude - cords1.latitude);
+  const dLat = toRadians(cords2.latitude - cords1.latitude);
+  const dLon = toRadians(cords2.longitude - cords1.longitude);
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRadians(cords1.latitude)) *
+      Math.cos(toRadians(cords2.latitude)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  const distance = earthRadiusKm * c;
+
+  return distance;
 };
