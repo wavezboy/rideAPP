4ddcd0737c622cbebbb96f8655dfca9f68ba6055
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-10-07 21:20:47 +0100

    ride completion controller in progress

--- src/controller/ride-controller.ts
+++ src/controller/ride-controller.ts
@@ -41,3 +41,21 @@ export const startRide: RequestHandler<
     next(error);
   }
 };
+
+export const completRide: RequestHandler<
+  rideReqBody,
+  unknown,
+  unknown,
+  unknown
+> = async (req, res, next) => {
+  const rideId = req.params.rideId;
+  try {
+    const ride = await rideModel.findOne({ rideId }).exec();
+
+    if (!ride) {
+      return res.status(404).json("ride not found");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
