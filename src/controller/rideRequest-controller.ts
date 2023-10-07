3c6cd66c2336fd60e7930b229ea389dff9b0fe36
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-10-07 07:57:24 +0100

    ride cancel done

--- src/controller/rideRequest-controller.ts
+++ src/controller/rideRequest-controller.ts
@@ -67,7 +67,7 @@ export const matchRequest: RequestHandler = async (req, res, next) => {
       },
     });
 
-    // Implement your logic to choose the best-matched driver
+    // Implement logic to choose the best-matched driver
     let bestMatchedDriver = await driverModel.findById({}).exec();
     let bestMatchedDistance = Number.MAX_VALUE;
 
@@ -78,7 +78,7 @@ export const matchRequest: RequestHandler = async (req, res, next) => {
 
       if (!(driverLocation && passengerLocation)) {
         throw CreateHttpError(
-          500,
+          404,
           "driver location or passenger location not available"
         );
       }
@@ -108,6 +108,39 @@ export const matchRequest: RequestHandler = async (req, res, next) => {
         .status(404)
         .json({ error: "no available driver within specified range" });
     }
+  } catch (error) {
+    next(error);
+  }
+};
+
+interface reqBody {
+  requestId: string;
+}
+
+export const cancelRequest: RequestHandler<
+  reqBody,
+  unknown,
+  unknown,
+  unknown
+> = async (req, res, next) => {
+  const passengerId = "";
+  const requestId = req.params.requestId;
+  try {
+    const rideRequest = await rideRequestModel.findOne({
+      passenger_id: passengerId,
+      _id: requestId,
+    });
+
+    if (!rideRequest) {
+      return res.status(404).json("ride request not found");
+    }
+
+    rideRequest.status = "canceled";
+
+    await rideRequest.save();
+    res
+      .status(200)
+      .json({ message: "ride request can canceled succesfully", rideRequest });
   } catch (error) {
     next(error);
   }
