bb7df2968977de2f06e93a4eeb74cfb19b53083d
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-10-19 13:04:50 +0100

    get review

--- src/routes/review-routes.ts
+++ src/routes/review-routes.ts
@@ -3,6 +3,7 @@ import * as reviewController from "../controller/review-controller";
 
 const router = express.Router();
 
-router.post("create-review", reviewController.createReview);
+router.post("/create-review", reviewController.createReview);
+router.get("/get-review", reviewController.getDriverRatings);
 
 export default router;
