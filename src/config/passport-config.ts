610b3b8de33e068887876e80d6ba67cb915857a1
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-10-22 10:28:48 +0100

    passport set up

--- src/config/passport-config.ts
+++ src/config/passport-config.ts
@@ -1,3 +1,8 @@
 import passport from "passport";
 import LocalStrategy from "passport-local";
 import userModel from "../models/userModel";
+import bcrypt from "bcrypt";
+
+passport.use();
+
+export default passport;
