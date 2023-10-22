fc2cbb3df20553d8b3df7953f3e8103955c4b012
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-10-22 11:57:55 +0100

    passport local imported correctly

--- src/config/passport-config.ts
+++ src/config/passport-config.ts
@@ -3,6 +3,13 @@ import { Strategy as LocalStrategy } from "passport-local";
 import userModel from "../models/userModel";
 import bcrypt from "bcrypt";
 
-passport.use(new LocalStrategy());
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    const user = await userModel.findOne({ username });
+    if (!user) {
+      return;
+    }
+  })
+);
 
 export default passport;
